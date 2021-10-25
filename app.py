ba59dba6498d6c864df3edf7567a0a2477f6873e
Helen Uzhegova <67826432+uzhegovaelena@users.noreply.github.com>
2021-10-25 18:34:46 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import asyncio
+
+import asyncpg
+from aiohttp import web
+
+from routes import setup_routes
+
+
+async def create_app():
+    app = web.Application()
+
+    app["db"] = await asyncpg.create_pool(
+        database="games",
+        user="postgres",
+        password="Ghysolk89h&",
+        host="localhost",
+        port=5432,
+    )
+
+    setup_routes(app)
+
+    return app
+
+
+def run_app(port):
+    loop = asyncio.get_event_loop()
+
+    app = loop.run_until_complete(create_app())
+
+    web.run_app(app, port=port)
+
+    try:
+        web.run_app(app)
+    except:
+        print("Something went wrong.")
+
+
+if __name__ == "__main__":
+    run_app(port=8080)
